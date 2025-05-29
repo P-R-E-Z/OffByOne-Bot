677fddd9aa8b4a8cd05b4b96dfa6425aaae171e9
Prez <154857421+P-R-E-Z@users.noreply.github.com>
2025-05-28 22:55:35 -0500

    Fixed so bot.py stuff

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 # Main entry point for the bot
-import botocore.eventstream
 import discord
 from discord.ext import commands
 from discord.ext.commands import bot
-
 from config import BOT_TOKEN
 from loguru import logger
 import asyncio
@@ -11,44 +9,37 @@ import asyncio
 from database import init_db
 
 intents = discord.Intents.all()
-boto3 = commands.Bot(command_prefix="!", intents=intents)
+Bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Core command modules
-CORE_COGS = [
-    "moderation",
-    "roles",
-    "toggles",
-    "applications"
-]
+CORE_COGS = ["moderation", "roles", "toggles", "applications"]
 
 # Feature command modules
-FEATURE_COGS = [
-    "hooks",
-    "updater",
-    "memes",
-    "coding_help",
-    "crossposter"
-]
+FEATURE_COGS = ["hooks", "updater", "memes", "coding_help", "crossposter"]
 
 ALL_COGS = CORE_COGS + FEATURE_COGS + TASK_COGS
 
-@bot.event 
+
+@bot.event
 async def on_ready():
-    logger.info(f"Logged in as {bot.user.name}")
+    logger.info(f"Logged in as {bot.user}")
+
 
 async def load_cogs():
     for cog in ALL_COGS:
         try:
-            modul = cog if cog.startswith("tasks.") else f"cogs.{cog}"
+            module = cog if cog.startswith("tasks.") else f"cogs.{cog}"
             await bot.load_extension(module)
             logger.success(f"Loaded cog: {cog}")
         except Exception as e:
             logger.error(f"Failed to load cog: {cog}: {e}")
-            
+
+
 async def main():
     await init_db()
     await load_cogs()
     await bot.start(BOT_TOKEN)
-    
+
+
 if __name__ == "__main__":
     asyncio.run(main())
